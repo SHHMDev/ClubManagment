09df7bfb50038015530098e4e32c55b032098a05
SHHMDev <shosseinidev@gmail.com>
2026-05-22 14:42:54 +0330

    firstVersion

---
+++ android/app/src/main/java/com/clubmanagment/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.clubmanagment.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import {AddTeacher} from './Components/AddTeacher.js'
+window.AddTeacher = AddTeacher;
+
+var app = document.getElementById("app");
+
+app.innerHTML = ``;
